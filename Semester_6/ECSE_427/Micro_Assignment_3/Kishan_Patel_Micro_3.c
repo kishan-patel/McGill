a04a7bfa4c8c696774043d8025870829ff506056
Kishan Patel <kishan@patelkishan.com>
2013-01-19 16:06:33 -0500

    Removed unused code.

--- Semester_6/ECSE_427/Micro_Assignment_3/Kishan_Patel_Micro_3.c
+++ Semester_6/ECSE_427/Micro_Assignment_3/Kishan_Patel_Micro_3.c
@@ -17,8 +17,7 @@ main()
 		dup2(fd[1],1);//Set stdout to the input side of the pipe.
 		close(fd[0]);//We never use the output side.
 		close(fd[1]);//Close the input side of the pipe.
-		char *argv[] = {"ls", NULL};//Command to be executed.
-		execlp("ls", "ls", NULL);//Execute the command
+		execlp("/bin/ls", "ls", NULL);//Execute the command
 	}
 	else
 	{//We're in the parent process.
@@ -26,8 +25,7 @@ main()
 		dup2(fd[0],0);//Set the stdin to the output side of the pipe.
 		close(fd[1]);//We never use the input side of the pipe.
 		close(fd[0]);//Close the output side of the pipe.
-		char *argv[] = {"wc","-l",NULL};//Command to be executed.
-		execlp("wc", "wc", "-l", NULL);//Execute the command.
+		execlp("/bin/wc", "wc", "-l", NULL);//Execute the command.
 	}
 	
 	return 0;
