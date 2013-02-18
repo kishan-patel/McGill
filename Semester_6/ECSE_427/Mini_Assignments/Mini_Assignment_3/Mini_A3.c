23839fc8e286ab91be04225b6f917a13ac9dd0f4
Kishan Patel <kishan@patelkishan.com>
2013-02-18 16:43:52 -0500

    Changed vector names.

--- Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
+++ Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
@@ -6,15 +6,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define REQ_NOT_MADE 	1
+#define	REQ_PENDING 	2
+#define	REQ_REJECTED	3
 int noProcess;
 int noResources;
 int i;
-int *resourceVector;
-int *available;
-int **max;
-int **need;
-int **allocated;
+int *availabilityVector;
+int **reqVector;
+int **needVector;
+int **holdVector;
+int *workVector;
+int *finishVector;
+int *processState;
+int terminate = 0;
 
+/*
+Vectors needed:
+REQ - N*M
+AVAIL - M
+NEED - N*M
+HOLD - N*M
+WORK - M
+FINISH - N
+*/
 initialize()
 {
 	//Number of processes and resources.
@@ -22,16 +37,25 @@ initialize()
 	noResources = 4;
 	
 	//Initialize to appropriate size.
-	resourceVector = (int *)malloc(sizeof(int)*noResources);
-	available = (int *)malloc(sizeof(int)*noResources);
-	max = (int **)malloc(sizeof(int *)*noResources);
-	need = (int **)malloc(sizeof(int *)*noResources);
-	allocated = (int **)malloc(sizeof(int *)*noResources);
+	availabilityVector = (int *)malloc(sizeof(int)*noResources);
+	reqVector = (int **)malloc(sizeof(int *)*noResources);
+	needVector = (int **)malloc(sizeof(int *)*noResources);
+	holdVector = (int **)malloc(sizeof(int *)*noResources);
+	workVector = (int *)malloc(sizeof(int)*noResources);
+	finishVector = (int *)malloc(sizeof(int)*noProcess);
 	for(i=0;i<noProcess;i++)
 	{
-		max[i] = (int *)malloc(sizeof(int)*noProcess);
-		need[i] = (int *)malloc(sizeof(int)*noProcess);
-		allocated[i] = (int *)malloc(sizeof(int)*noProcess);
+		reqVector[i] = (int *)malloc(sizeof(int)*noProcess);
+		needVector[i] = (int *)malloc(sizeof(int)*noProcess);
+		holdVector[i] = (int *)malloc(sizeof(int)*noProcess);
+	}
+	processState = (int *)malloc(sizeof(int)*noProcess);
+	
+	//Initialize the state of each process. Intially, none of the processes have
+	//made any requests
+	for(i=0;i<noProcess;i++)
+	{
+		processState[i] = REQ_NOT_MADE;
 	}
 	
 	//Initialse the arrays to the appropriate values.
@@ -41,8 +65,7 @@ initialize()
 	{
 		for(noR=0;noR<noResources;noR++)
 		{
-			max[noP][noR] = (rand() % 8) + 5;
-			need[noP][noR] = max[noP][noR];
+			needVector[noP][noR] = (rand() % 8) + 5;
 		}
 	}
 	
@@ -51,19 +74,42 @@ initialize()
 		sumOfMaxResources = 0;
 		for(noP=0;noP<noProcess;noP++)
 		{
-			sumOfMaxResources += max[noP][noR];
+			sumOfMaxResources += needVector[noP][noR];
 		}
-		resourceVector[noR] = 0.6*sumOfMaxResources;
-		available[noR] = resourceVector[noR];
+		availabilityVector[noR] = 0.6*sumOfMaxResources;
 	}
 }
 
 void generateRequest()
 {
+
 }
 
 void verifyRequest()
 {
+	//Check if the requests of all the processes have been rejected. If 
+	//so, we print the statistics and end the program.
+	terminate = 1;
+	for(i=0;i<noProcess;i++)
+	{
+		if(processState[i]!=REQ_REJECTED)
+		{
+			terminate = 0;
+			break;
+		}
+	}
+	if(terminate)
+		return;
+		
+	//Pre-requisites before checking if system will be in safe state after
+	//granting resources.
+	
+	
+}
+
+int isSafe()
+{
+
 }
 
 void updateSystemState()
