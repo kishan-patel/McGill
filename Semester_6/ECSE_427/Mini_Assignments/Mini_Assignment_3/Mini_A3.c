136e44f029e693f15d824105c9fb8a1cebaa9994
Kishan Patel <kishan@patelkishan.com>
2013-02-18 14:04:27 -0500

    Initialized arrays.

--- Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
+++ Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
@@ -6,16 +6,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void setup()
+int noProcess;
+int noResources;
+int i;
+int *resourceVector;
+int *available;
+int **max;
+int **need;
+int **allocated;
+
+initialize()
+{
+	//Number of processes and resources.
+	noProcess = (rand() % 6) + 5; //noProc in the range 5-10
+	noResources = 4;
+	
+	//Initialize to appropriate size.
+	resourceVector = (int *)malloc(sizeof(int)*noResources);
+	available = (int *)malloc(sizeof(int)*noResources);
+	max = (int **)malloc(sizeof(int *)*noResources);
+	need = (int **)malloc(sizeof(int *)*noResources);
+	allocated = (int **)malloc(sizeof(int *)*noResources);
+	for(i=0;i<noProcess;i++)
+	{
+		max[i] = (int *)malloc(sizeof(int)*noProcess);
+		need[i] = (int *)malloc(sizeof(int)*noProcess);
+		allocated[i] = (int *)malloc(sizeof(int)*noProcess);
+	}
+	
+	//Initialse the arrays to the appropriate values.
+	int noP = 0, noR = 0;
+	int sumOfMaxResources = 0;
+	for(noP=0;noP<noProcess;noP++)
+	{
+		for(noR=0;noR<noResources;noR++)
+		{
+			max[noP][noR] = (rand() % 8) + 5;
+			need[noP][noR] = max[noP][noR];
+		}
+	}
+	
+	for(noR=0;noR<noResources;noR++)
+	{
+		sumOfMaxResources = 0;
+		for(noP=0;noP<noProcess;noP++)
+		{
+			sumOfMaxResources += max[noP][noR];
+		}
+		resourceVector[noR] = 0.6*sumOfMaxResources;
+		available[noR] = resourceVector[noR];
+	}
+}
+
+void generateRequest()
 {
 }
 
-int main(int argc,char[] argv)
+void verifyRequest()
 {
-	int resourceVector[4];
-	int max[5][4];
-	int allocated[5][4];
-	int available[4];
-	int need[5][4];
-	
+}
+
+void updateSystemState()
+{
+}
+
+void printStatistics()
+{
+}
+
+int main(int argc,char *argv[])
+{
+	initialize();
 }
