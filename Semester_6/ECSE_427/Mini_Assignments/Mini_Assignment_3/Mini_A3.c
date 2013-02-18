f3913b63631c87dadbbab154ea8e129c5941f454
Kishan Patel <kishan@patelkishan.com>
2013-02-18 12:23:55 -0500

    Adding mini assignment 3.

---
+++ Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
@@ -0,0 +1,21 @@
+/***********************************************************/
+/*Kishan Patel											   */
+/*260376121       										   */
+/*Mini Assingment 3					                       */
+/***********************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void setup()
+{
+}
+
+int main(int argc,char[] argv)
+{
+	int resourceVector[4];
+	int max[5][4];
+	int allocated[5][4];
+	int available[4];
+	int need[5][4];
+	
+}
