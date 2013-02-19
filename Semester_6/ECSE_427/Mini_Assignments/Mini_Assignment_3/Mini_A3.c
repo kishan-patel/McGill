520096973c1c3ff69b82a0d51cbaad51424e4082
Kishan Patel <kishan@patelkishan.com>
2013-02-19 09:36:52 -0500

    Added function prototypes. Requires testing.

--- Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
+++ Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
@@ -1,25 +1,24 @@
 /***********************************************************/
-/*Kishan Patel											   */
-/*260376121       										   */
-/*Mini Assingment 3					                       */
+/*Kishan Patel											  										 */
+/*260376121       										   									 */
+/*Mini Assingment 3					                       				 */
 /***********************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
-#define REQ_NOT_MADE 	1
-#define	REQ_PENDING 	2
-#define	REQ_REJECTED	3
+#define SAFE_STATE		1
+#define UNSAFE_STATE 	2
 int noProcess;
 int noResources;
-int i;
+int i,j;
 int *availabilityVector;
 int **reqVector;
 int **needVector;
 int **holdVector;
 int *workVector;
 int *finishVector;
-int *processState;
-int terminate = 0;
+int allRequestsSatisfied = 1;
+int status;
 
 /*
 Vectors needed:
@@ -30,7 +29,30 @@ HOLD - N*M
 WORK - M
 FINISH - N
 */
-initialize()
+void initialize();
+void generateRequest();
+int processRequest();
+int isSafe();
+void printStatistics();
+
+
+
+int main(int argc,char *argv[])
+{
+	initialize();
+	//printStatistics();
+	do{
+		if(allRequestsSatisfied)
+		{
+			generateRequest();
+			allRequestsSatisfied = 0;
+		}
+		status = processRequest();	
+	}while(status != UNSAFE_STATE);
+	printStatistics();
+}
+
+void initialize()
 {
 	//Number of processes and resources.
 	noProcess = (rand() % 6) + 5; //noProc in the range 5-10
@@ -49,15 +71,13 @@ initialize()
 		needVector[i] = (int *)malloc(sizeof(int)*noProcess);
 		holdVector[i] = (int *)malloc(sizeof(int)*noProcess);
 	}
-	processState = (int *)malloc(sizeof(int)*noProcess);
-	
-	//Initialize the state of each process. Intially, none of the processes have
-	//made any requests
-	for(i=0;i<noProcess;i++)
-	{
-		processState[i] = REQ_NOT_MADE;
-	}
-	
+	generateRequest();
+}
+
+void generateRequest()
+{
+	//Set the request vector and need vectors. The request vector will
+	//not change throught the course of the program.
 	//Initialse the arrays to the appropriate values.
 	int noP = 0, noR = 0;
 	int sumOfMaxResources = 0;
@@ -68,7 +88,6 @@ initialize()
 			needVector[noP][noR] = (rand() % 8) + 5;
 		}
 	}
-	
 	for(noR=0;noR<noResources;noR++)
 	{
 		sumOfMaxResources = 0;
@@ -80,47 +99,109 @@ initialize()
 	}
 }
 
-void generateRequest()
-{
-
-}
-
-void verifyRequest()
-{
-	//Check if the requests of all the processes have been rejected. If 
-	//so, we print the statistics and end the program.
-	terminate = 1;
+int processRequest()
+{	
+	//Attempt to statisfy the resource request made by a particular process. If no 
+	//such safe state exist, we terminate the algorithm.
+	int reqAttempted = 0;
 	for(i=0;i<noProcess;i++)
 	{
-		if(processState[i]!=REQ_REJECTED)
+		for(j=0;j<noResources;j++)
 		{
-			terminate = 0;
-			break;
+			if(needVector[i][j] <= availabilityVector[j] && needVector[i][j] != 0)
+			{
+				int oldAvail = availabilityVector[j];
+				int oldHold = holdVector[i][j];
+				int oldNeed = needVector[i][j];
+				availabilityVector[j] -= needVector[i][j];
+				holdVector[i][j] += needVector[i][j];
+				needVector[i][j] -= needVector[i][j];
+				if(isSafe(i,j) == SAFE_STATE)
+				{
+					//resourceGranted = 1;
+					availabilityVector[j] += holdVector[i][j];
+					holdVector[i][j] = 0;
+					needVector[i][j] = 0;
+					return SAFE_STATE; //We have fulfilled this request so now we start over.
+				}
+				else
+				{
+					//Just ignore this request and process the other ones.
+					reqAttempted = 1;
+					availabilityVector[j] = oldAvail;
+					holdVector[i][j] = oldHold;
+					needVector[i][j] = oldNeed;
+				}
+			}
 		}
 	}
-	if(terminate)
-		return;
-		
-	//Pre-requisites before checking if system will be in safe state after
-	//granting resources.
 	
-	
+	if(reqAttempted)
+	{	//This is the case where there are still pending requests that cannot
+		//be fullfilled.
+		return UNSAFE_STATE;
+	}else{
+		//All of the requests of the proceses have been satisfied so we terminate.
+		allRequestsSatisfied = 1;
+		return SAFE_STATE;
+	}
 }
 
 int isSafe()
 {
-
+	
+	for(j=0;j<noResources;j++)
+	{
+		workVector[j] = availabilityVector[j];
+	}
+	for(i=0;i<noProcess;i++)
+	{
+		finishVector[i] = 0;
+	}
+	int processFinish = 0;
+	for(i=0;i<noResources;i++)
+	{
+		for(j=0;j<noProcess;j++)
+		{
+			if(needVector[i][j] <= workVector[j] && finishVector[i] == 0)
+			{
+				finishVector[0] = 1;
+				workVector[j] += holdVector[i][j];
+				processFinish++;
+			}
+		}
+	}
+	if(processFinish == noProcess)
+	{
+		return SAFE_STATE;
+	}
+	else
+	{
+		return UNSAFE_STATE;
+	}
 }
 
-void updateSystemState()
-{
-}
 
 void printStatistics()
 {
-}
-
-int main(int argc,char *argv[])
-{
-	initialize();
+	printf("Hold Matrix\n");
+	for(i=0;i<noProcess;i++)
+	{
+		for(j=0;j<noResources;j++)
+		{
+			printf("%d\t",holdVector[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\n");
+	printf("Need Matrix\n");
+	for(i=0;i<noProcess;i++)
+	{
+		for(j=0;j<noResources;j++)
+		{
+			printf("%d\t",needVector[i][j]);
+		}
+		printf("\n");
+	}
 }
