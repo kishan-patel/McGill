86a75068639f23c339306ab967e759d92776930d
Kishan Patel <kishan@patelkishan.com>
2013-02-19 17:52:24 -0500

    Hold matrices is 0 at the end. Need to look into this.

--- Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
+++ Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
@@ -6,50 +6,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Variable declarations.
 #define SAFE_STATE		1
 #define UNSAFE_STATE 	2
+#define MAX_PROCESS 	12
+#define MAX_RESOURCE	4
 int noProcess;
 int noResources;
-int i,j;
-int *availabilityVector;
-int **reqVector;
-int **needVector;
-int **holdVector;
-int *workVector;
-int *finishVector;
+int availabilityVector[MAX_RESOURCE];
+int reqVector[MAX_PROCESS][MAX_RESOURCE];
+int needVector[MAX_PROCESS][MAX_RESOURCE];
+int holdVector[MAX_PROCESS][MAX_RESOURCE];
+int workVector[MAX_RESOURCE];
+int finishVector[MAX_PROCESS];
 int allRequestsSatisfied = 1;
 int status;
 
-/*
-Vectors needed:
-REQ - N*M
-AVAIL - M
-NEED - N*M
-HOLD - N*M
-WORK - M
-FINISH - N
-*/
+//Function prototypes.
 void initialize();
-void generateRequest();
+void generateRandomRequest();
 int processRequest();
 int isSafe();
 void printStatistics();
-
-
+void generatePassingExample();
+void generateFailingExample();
 
 int main(int argc,char *argv[])
 {
-	initialize();
-	//printStatistics();
+	int iteration = 1;
+	initialize();//Just sets the number of processes and resources.
 	do{
 		if(allRequestsSatisfied)
 		{
-			generateRequest();
+			generateRandomRequest();//If the request are satisfied, we generate a new random request.
+			//generatePassingExample();	//For testing purposes. Comment out the other two.
+			//generateFailingExample();	//For testing purposes. Comment out the other two.
 			allRequestsSatisfied = 0;
+			printf("Resetting\n");
+			printStatistics();
+			printf("\n\n");
 		}
 		status = processRequest();	
+		printf("Iteration %d Results\n",iteration);
+		printStatistics();
 	}while(status != UNSAFE_STATE);
-	printStatistics();
+	 printStatistics();
 }
 
 void initialize()
@@ -57,24 +58,9 @@ void initialize()
 	//Number of processes and resources.
 	noProcess = (rand() % 6) + 5; //noProc in the range 5-10
 	noResources = 4;
-	
-	//Initialize to appropriate size.
-	availabilityVector = (int *)malloc(sizeof(int)*noResources);
-	reqVector = (int **)malloc(sizeof(int *)*noResources);
-	needVector = (int **)malloc(sizeof(int *)*noResources);
-	holdVector = (int **)malloc(sizeof(int *)*noResources);
-	workVector = (int *)malloc(sizeof(int)*noResources);
-	finishVector = (int *)malloc(sizeof(int)*noProcess);
-	for(i=0;i<noProcess;i++)
-	{
-		reqVector[i] = (int *)malloc(sizeof(int)*noProcess);
-		needVector[i] = (int *)malloc(sizeof(int)*noProcess);
-		holdVector[i] = (int *)malloc(sizeof(int)*noProcess);
-	}
-	generateRequest();
 }
 
-void generateRequest()
+void generateRandomRequest()
 {
 	//Set the request vector and need vectors. The request vector will
 	//not change throught the course of the program.
@@ -85,9 +71,13 @@ void generateRequest()
 	{
 		for(noR=0;noR<noResources;noR++)
 		{
+			//Hold is taken to be 0 and the need for 
+			//a given process is a random number between 5-12.
 			needVector[noP][noR] = (rand() % 8) + 5;
+			reqVector[noP][noR] = needVector[noP][noR];
 		}
 	}
+
 	for(noR=0;noR<noResources;noR++)
 	{
 		sumOfMaxResources = 0;
@@ -95,7 +85,9 @@ void generateRequest()
 		{
 			sumOfMaxResources += needVector[noP][noR];
 		}
-		availabilityVector[noR] = 0.6*sumOfMaxResources;
+		//The availability of any given resource is 0.6*the total need for that
+		//resources for all the processes participating.
+		availabilityVector[noR] = 0.2*sumOfMaxResources;
 	}
 }
 
@@ -104,22 +96,26 @@ int processRequest()
 	//Attempt to statisfy the resource request made by a particular process. If no 
 	//such safe state exist, we terminate the algorithm.
 	int reqAttempted = 0;
+	int i,j;
 	for(i=0;i<noProcess;i++)
 	{
 		for(j=0;j<noResources;j++)
 		{
 			if(needVector[i][j] <= availabilityVector[j] && needVector[i][j] != 0)
 			{
+				//This is a provisional allocation. If allocating resources in this manner
+				//will result in an unsafe state, we have to revert back. Thus, I'm storing
+				//the current value in temporary variables.
 				int oldAvail = availabilityVector[j];
 				int oldHold = holdVector[i][j];
 				int oldNeed = needVector[i][j];
 				availabilityVector[j] -= needVector[i][j];
 				holdVector[i][j] += needVector[i][j];
 				needVector[i][j] -= needVector[i][j];
-				if(isSafe(i,j) == SAFE_STATE)
+				if(isSafe() == SAFE_STATE)
 				{
-					//resourceGranted = 1;
-					availabilityVector[j] += holdVector[i][j];
+					//Release the resources before proceeding.
+					availabilityVector[j] = availabilityVector[j]+holdVector[i][j];
 					holdVector[i][j] = 0;
 					needVector[i][j] = 0;
 					return SAFE_STATE; //We have fulfilled this request so now we start over.
@@ -149,7 +145,8 @@ int processRequest()
 
 int isSafe()
 {
-	
+	int i,j;
+	int processFinish = 0;
 	for(j=0;j<noResources;j++)
 	{
 		workVector[j] = availabilityVector[j];
@@ -158,15 +155,14 @@ int isSafe()
 	{
 		finishVector[i] = 0;
 	}
-	int processFinish = 0;
-	for(i=0;i<noResources;i++)
+	for(i=0;i<noProcess;i++)
 	{
-		for(j=0;j<noProcess;j++)
+		for(j=0;j<noResources;j++)
 		{
 			if(needVector[i][j] <= workVector[j] && finishVector[i] == 0)
 			{
-				finishVector[0] = 1;
-				workVector[j] += holdVector[i][j];
+				finishVector[i] = 1;
+				workVector[j] = workVector[j] +  holdVector[i][j];
 				processFinish++;
 			}
 		}
@@ -184,7 +180,18 @@ int isSafe()
 
 void printStatistics()
 {
-	printf("Hold Matrix\n");
+	int i,j;
+	printf("Orignal Request Matrix\n");
+	for(i=0;i<noProcess;i++)
+	{
+		for(j=0;j<noResources;j++)
+		{
+			printf("%d\t",reqVector[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nHold Matrix\n");
 	for(i=0;i<noProcess;i++)
 	{
 		for(j=0;j<noResources;j++)
@@ -194,8 +201,7 @@ void printStatistics()
 		printf("\n");
 	}
 	
-	printf("\n");
-	printf("Need Matrix\n");
+	printf("\nNeed Matrix\n");
 	for(i=0;i<noProcess;i++)
 	{
 		for(j=0;j<noResources;j++)
@@ -204,4 +210,42 @@ void printStatistics()
 		}
 		printf("\n");
 	}
+	
+	printf("\nAvailability Matrix\n");
+	for(i=0;i<noResources;i++)
+	{
+		printf("%d\n",availabilityVector[i]);
+	}
+}
+
+void generatePassingExample()
+{
+	//A passing example from the class notes.
+	reqVector[0][0] = 5;
+	reqVector[1][0] = 4;
+	reqVector[2][0] = 2;
+	holdVector[0][0] = 3;
+	holdVector[1][0] = 1;
+	holdVector[2][0] = 1;
+	needVector[0][0] = 2;
+	needVector[1][0] = 3;
+	needVector[2][0] = 1;
+	availabilityVector[0] = 2;
+	workVector[0] = 2;
+}
+
+void generateFailingExample()
+{
+	//A failing example from the class notes.
+	reqVector[0][0] = 5;
+	reqVector[1][0] = 5;
+	reqVector[2][0] = 2;
+	holdVector[0][0] = 2;
+	holdVector[1][0] = 2;
+	holdVector[2][0] = 1;
+	needVector[0][0] = 3;
+	needVector[1][0] = 3;
+	needVector[1][0] = 1;
+	availabilityVector[0] = 2;
+	workVector[0] = 2;
 }
