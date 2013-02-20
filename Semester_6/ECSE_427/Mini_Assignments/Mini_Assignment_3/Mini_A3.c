7b6e24a7db3d5d513ee778c99d60c18e87e97d74
Kishan Patel <kishan@patelkishan.com>
2013-02-19 21:08:43 -0500

    Considering 4 resources at a time for each process.

--- Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
+++ Semester_6/ECSE_427/Mini_Assignments/Mini_Assignment_3/Mini_A3.c
@@ -14,9 +14,10 @@
 int noProcess;
 int noResources;
 int availabilityVector[MAX_RESOURCE];
-int reqVector[MAX_PROCESS][MAX_RESOURCE];
+int maxVector[MAX_PROCESS][MAX_RESOURCE];
 int needVector[MAX_PROCESS][MAX_RESOURCE];
 int holdVector[MAX_PROCESS][MAX_RESOURCE];
+int reqVector[MAX_PROCESS][MAX_RESOURCE];
 int workVector[MAX_RESOURCE];
 int finishVector[MAX_PROCESS];
 int allRequestsSatisfied = 1;
@@ -43,23 +44,26 @@ int main(int argc,char *argv[])
 				printf("Solved Problem. Will generate new one.\n");
 				printStatistics();
 				printf("\n\n");
+				iteration = 0;
 			}
 			generateRandomRequest();//If the request are satisfied, we generate a new random request.
 			//generatePassingExample();	//For testing purposes. Comment out the other two.
 			//generateFailingExample();	//For testing purposes. Comment out the other two.
 			allRequestsSatisfied = 0;
 		}
-		status = processRequest();	
+		status = processRequest();
+		printf("Iteration %d. Results.\n",iteration);
+		printStatistics();
+		printf("\n\n");
 		iteration++;
 	}while(status != UNSAFE_STATE);
 	printf("Unsolved Problem. Program will terminate.\n");
-	printStatistics();
 }
 
 void initialize()
 {
 	//Number of processes and resources.
-	noProcess = (rand() % 6) + 5; //noProc in the range 5-10
+	noProcess = (rand() % 4) ;//+ 5; //noProc in the range 5-10
 	noResources = 4;
 }
 
@@ -76,8 +80,9 @@ void generateRandomRequest()
 		{
 			//Hold is taken to be 0 and the need for 
 			//a given process is a random number between 5-12.
-			needVector[noP][noR] = (rand() % 8) + 5;
-			reqVector[noP][noR] = needVector[noP][noR];
+			maxVector[noP][noR] = (rand() % 8) + 5;
+			needVector[noP][noR] = maxVector[noP][noR];
+			reqVector[noP][noR] = (needVector[noP][noR]%2)+1;
 		}
 	}
 
@@ -90,8 +95,11 @@ void generateRandomRequest()
 		}
 		//The availability of any given resource is 0.6*the total need for that
 		//resources for all the processes participating.
-		availabilityVector[noR] = 0.2*sumOfMaxResources;
+		availabilityVector[noR] = 0.7*sumOfMaxResources;
 	}
+	printf("Original Problem\n");
+	printStatistics();
+	printf("\n\n");
 }
 
 int processRequest()
@@ -102,48 +110,128 @@ int processRequest()
 	int i,j;
 	for(i=0;i<noProcess;i++)
 	{
-		for(j=0;j<noResources;j++)
-		{
-			if(needVector[i][j] <= availabilityVector[j] && needVector[i][j] != 0)
-			{
-				//This is a provisional allocation. If allocating resources in this manner
-				//will result in an unsafe state, we have to revert back. Thus, I'm storing
-				//the current value in temporary variables.
-				int oldAvail = availabilityVector[j];
-				int oldHold = holdVector[i][j];
-				int oldNeed = needVector[i][j];
-				availabilityVector[j] -= needVector[i][j];
-				holdVector[i][j] += needVector[i][j];
-				needVector[i][j] -= needVector[i][j];
-				if(isSafe() == SAFE_STATE)
+		//This is a provisional allocation. If allocating resources in this manner
+		//will result in an unsafe state, we have to revert back. Thus, I'm storing
+		//the current value in temporary variables.
+		if(reqVector[i][0] <= availabilityVector[0]
+				&&reqVector[i][1] <= availabilityVector[1]
+					&&reqVector[i][2] <= availabilityVector[2]
+						&&reqVector[i][3] <= availabilityVector[3]){
+			
+			//Resource 1
+			int oldAvail1 = availabilityVector[0];
+			int oldHold1 = holdVector[i][0];
+			int oldNeed1 = needVector[i][0];
+			availabilityVector[0] -= reqVector[i][0];
+			holdVector[i][0] += reqVector[i][0];
+			needVector[i][0] -= (reqVector[i][0]>needVector[i][0])?0:reqVector[i][0];
+			//reqVector[i][0] = needVector[i][0] - holdVector[i][0];
+			
+			//Resource 2
+			int oldAvail2 = availabilityVector[1];
+			int oldHold2 = holdVector[i][1];
+			int oldNeed2 = needVector[i][1];
+			availabilityVector[1] -= reqVector[i][1];
+			holdVector[i][1] += reqVector[i][1];
+			needVector[i][1] -= (reqVector[i][1]>needVector[i][1])?0:reqVector[i][1];
+		//	reqVector[i][1] = needVector[i][1] - holdVector[i][1];
+
+			
+			//Resource 3
+			int oldAvail3 = availabilityVector[2];
+			int oldHold3 = holdVector[i][2];
+			int oldNeed3 = needVector[i][2];
+			availabilityVector[2] -= reqVector[i][2];
+			holdVector[i][2] += reqVector[i][2];
+			needVector[i][2] -= (reqVector[i][2]>needVector[i][2])?0:reqVector[i][2];
+			//reqVector[i][2] = needVector[i][2] - holdVector[i][2];
+
+			
+			//Resource 4
+			int oldAvail4 = availabilityVector[3];
+			int oldHold4 = holdVector[i][3];
+			int oldNeed4 = needVector[i][3];
+			availabilityVector[3] -= reqVector[i][3];
+			holdVector[i][3] += reqVector[i][3];
+			needVector[i][3] -= (reqVector[i][3]>needVector[i][3])?0:reqVector[i][3];
+			//reqVector[i][3] = needVector[i][3] - holdVector[i][3];
+
+
+			if(isSafe() == SAFE_STATE)
 				{
-					//Release the resources before proceeding.
-					availabilityVector[j] = availabilityVector[j]+holdVector[i][j];
-					holdVector[i][j] = 0;
-					needVector[i][j] = 0;
+
+					if(needVector[i][0] == 0 && needVector[i][0] == 0 
+							&& needVector[i][0] == 0 && needVector[i][0] == 0)
+					{
+						//Resource 1
+						availabilityVector[0] = holdVector[i][0];
+						holdVector[i][0] = 0;
+						needVector[i][0] = 0;
+						reqVector[i][0] = 0;
+						
+						//Resource 2
+						availabilityVector[1] = holdVector[i][1];
+						holdVector[i][1] = 0;
+						needVector[i][1] = 0;
+						reqVector[i][1] = 0;
+						
+						//Resource 3
+						availabilityVector[2] = holdVector[i][2];
+						holdVector[i][2] = 0;
+						needVector[i][2] = 0;
+						reqVector[i][2] = 0;
+						
+						//Resource 4
+						availabilityVector[3] = holdVector[i][3];
+						holdVector[i][3] = 0;
+						needVector[i][3] = 0;
+						reqVector[i][3] = 0;
+					}
+					
 					return SAFE_STATE; //We have fulfilled this request so now we start over.
 				}
 				else
 				{
-					//Just ignore this request and process the other ones.
+					//Just ignore this request and process the other ones. Reset the value first.
 					reqAttempted = 1;
-					availabilityVector[j] = oldAvail;
-					holdVector[i][j] = oldHold;
-					needVector[i][j] = oldNeed;
-				}
-			}
+					//Resource 1
+					availabilityVector[0] = oldAvail1;
+					holdVector[i][0] = oldHold1;
+					needVector[i][0] = oldNeed1;
+					
+					//Resource 2
+					availabilityVector[1] = oldAvail2;
+					holdVector[i][1] = oldHold2;
+					needVector[i][1] = oldNeed2;
+					
+					//Resource 3
+					availabilityVector[2] = oldAvail3;
+					holdVector[i][2] = oldHold3;
+					needVector[i][2] = oldNeed3;
+					
+					//Resource 4
+					availabilityVector[3] = oldAvail4;
+					holdVector[i][3] = oldHold4;
+					needVector[i][3] = oldNeed4;
+				}	
 		}
-	}
-	
+		else
+		{
+			continue;
+		}
+	}		
 	if(reqAttempted)
 	{	//This is the case where there are still pending requests that cannot
 		//be fullfilled.
 		return UNSAFE_STATE;
-	}else{
+	}
+	else
+	{
 		//All of the requests of the proceses have been satisfied so we terminate.
 		allRequestsSatisfied = 1;
 		return SAFE_STATE;
 	}
+	
 }
 
 int isSafe()
@@ -160,14 +248,24 @@ int isSafe()
 	}
 	for(i=0;i<noProcess;i++)
 	{
-		for(j=0;j<noResources;j++)
+		if(needVector[i][0] <= workVector[0] && needVector[i][1] <= workVector[1]
+					&& needVector[i][2] <= workVector[2] && needVector[i][3] <= workVector[3]
+						&& finishVector[i] == 0)
 		{
-			if(needVector[i][j] <= workVector[j] && finishVector[i] == 0)
-			{
 				finishVector[i] = 1;
-				workVector[j] = workVector[j] +  holdVector[i][j];
 				processFinish++;
-			}
+				
+				//Resource 1
+				workVector[0] = workVector[0] +  holdVector[i][0];
+
+				//Resource 2
+				workVector[1] = workVector[1] +  holdVector[i][1];
+				
+				//Resource 3
+				workVector[2] = workVector[2] +  holdVector[i][2];
+				
+				//Resource 4
+				workVector[3] = workVector[3] +  holdVector[i][3];
 		}
 	}
 	if(processFinish == noProcess)
@@ -189,7 +287,7 @@ void printStatistics()
 	{
 		for(j=0;j<noResources;j++)
 		{
-			printf("%d\t",reqVector[i][j]);
+			printf("%d\t",maxVector[i][j]);
 		}
 		printf("\n");
 	}
