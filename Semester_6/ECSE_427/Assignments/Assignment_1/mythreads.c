9745b80a6403ba414f3d708bdd882ee6c9776649
Kishan Patel <kishan@patelkishan.com>
2013-03-12 23:07:47 -0400

    Adding main file for assignment 1.

---
+++ Semester_6/ECSE_427/Assignments/Assignment_1/mythreads.c
@@ -0,0 +1,20 @@
+/***********************************************************/
+/*Kishan Patel											  										 */
+/*260376121       										   									 */
+/*Assignment 1      				                       				 */
+/***********************************************************/
+#include "list.h"
+
+/*Global Variables*/
+List *runQueue;
+
+/*Function Prototypes*/
+int mythread_init();
+
+/*
+This function initializes all the globa data structures for the thread system
+*/
+int mythread_init()
+{
+  
+}
