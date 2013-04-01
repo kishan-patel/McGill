57547690c307daf888ed8d4c1373e93b8e378a44
Kishan Patel <kishan@patelkishan.com>
2013-04-01 08:52:02 -0400

    Added comments.

--- Semester_6/ECSE_427/Assignments/Assignment_2/mymalloc.c
+++ Semester_6/ECSE_427/Assignments/Assignment_2/mymalloc.c
@@ -8,123 +8,152 @@
 #define ALLOCATED  	    100
 #define EXTRA_MEMORY 	  128*1024
 
+/*Allways points to the beginning of the free list. Was used for debuggin purposes.*/
 static block_info* fixedHead;
+/*Points to the first free block in the free list.*/
 static block_info* head;
+/*Points to the last free block in the free list.*/
 static block_info* tail;
+/*Flag to keep a track of whether a block was initialized for the first time.*/
 static int initialized = 0;
+/*Policy to use while allocating memory.*/
 static int policyToUse = FIRST_FIT;
+/*Flag to keep track of the total number of bytes allocated thus far (includes meta blocks).*/
 static int totalBytesAllocated = 0;
+/*Flag to keep track of the total number of free bytes in the free list (not counting meta blocks).*/
 static int totalFreeBytes = 0;
+/*Flag to keep track of the largest free block in the free list.*/
 static int largestFreeSpace = 0;
 
+/*
+This function returns a pointer to a memory block that is at least as large as 
+the requested size. It uses either the First-Fit or Best-Fit policy to
+allocated a block of memory. If there is not enough memory in the free list it creates
+more memory using the sbrk call.
+*/
 void* my_malloc(int requestedSize)
 {
-    block_info* block;
+  block_info* block;
 
-    if(!initialized)
-    {
-      initialized = 1;
+  if(!initialized)
+  {
+    initialized = 1;
 
-      block_info* topBlock = (block_info *)sbrk(sizeof(block_info));
-      topBlock->address = (void *)sbrk(requestedSize + EXTRA_MEMORY);
-      topBlock->availability = FREE;
-      topBlock->size = requestedSize + EXTRA_MEMORY;
+    //Setup the top meta block.
+    block_info* topBlock = (block_info *)sbrk(sizeof(block_info));
+    topBlock->address = (void *)sbrk(requestedSize + EXTRA_MEMORY);
+    topBlock->availability = FREE;
+    topBlock->size = requestedSize + EXTRA_MEMORY;
 
-      block_info* bottomBlock = (block_info *)sbrk(sizeof(block_info));
-      bottomBlock -> address = topBlock ->address;
-      bottomBlock -> availability = FREE;
-      bottomBlock -> size = requestedSize + EXTRA_MEMORY;
-      
-      totalBytesAllocated += sizeof(block_info) + requestedSize + EXTRA_MEMORY + sizeof(block_info);
-      totalFreeBytes += requestedSize + EXTRA_MEMORY;
-      largestFreeSpace = requestedSize + EXTRA_MEMORY;
-      fixedHead = topBlock;
-      head = topBlock;
-      tail = topBlock;
-    }
+    //Setup the bottom meta block.
+    block_info* bottomBlock = (block_info *)sbrk(sizeof(block_info));
+    bottomBlock -> address = topBlock ->address;
+    bottomBlock -> availability = FREE;
+    bottomBlock -> size = requestedSize + EXTRA_MEMORY;
 
-    block = head;
-    if(policyToUse == FIRST_FIT)
-    {
-      while(block)
-      {
-         if(block->size > requestedSize && block->availability == FREE)
-         {
-             //Case 1: It is not possible to split the free block into a smaller portion
-             //as there won't be enough memory to store the information about the free
-             //block. Thus, we return the block with excess memory.
-             if(block->size - requestedSize <= 2*sizeof(block_info))
-             {
-               return allocateExtraMemory(block,requestedSize);
-             }
-
-             //Case 2: We can allocated the requested memory and there is still enough memory
-             //left to store information about the segmented block size. Thus, we partition
-             //the available memory.
-             else
-             {
-                return allocateMemoryAndStoreRemaining(block,requestedSize);
-             }
-         }
-         
-         else
-         {
-           if(block->next == 0)
-           {
-             return createMoreMemory(block,requestedSize);
-           }
-           else
-           {
-             block = block->next;
-           }
-         }
-      }
-    }
+    //Setup the initial pointers to the free list.
+    fixedHead = topBlock;
+    head = topBlock;
+    tail = topBlock;
     
-    else
+    //Store information for later printing.
+    totalBytesAllocated += sizeof(block_info) + requestedSize + EXTRA_MEMORY + sizeof(block_info);
+    totalFreeBytes += requestedSize + EXTRA_MEMORY;
+    largestFreeSpace = requestedSize + EXTRA_MEMORY;
+  }
+
+  block = head;
+  if(policyToUse == FIRST_FIT)
+  {
+    while(block)
     {
-      block_info* bestFit = block;
-      int foundAFit = 0;
-      
-      while(block){
-        if(block->size >= requestedSize && block->availability==FREE){
-          if(block -> size < bestFit -> size || bestFit -> size < requestedSize){
-            bestFit = block;
-            foundAFit = 1;
-          }
-        }
-        block = block -> next;
-      }
-      
-      if(foundAFit)
+      if(block->size > requestedSize && block->availability == FREE)
       {
         //Case 1: It is not possible to split the free block into a smaller portion
         //as there won't be enough memory to store the information about the free
         //block. Thus, we return the block with excess memory.
-        if(bestFit->size - requestedSize <= 2*sizeof(block_info))
+        if(block->size - requestedSize <= 2*sizeof(block_info))
         {
-          return allocateExtraMemory(bestFit,requestedSize);
-          
+          return allocateExtraMemory(block,requestedSize);
         }
 
        //Case 2: We can allocated the requested memory and there is still enough memory
        //left to store information about the segmented block size. Thus, we partition
        //the available memory.
-       else
-       {
-          return allocateMemoryAndStoreRemaining(bestFit,requestedSize);
-       }
-        
+        else
+        {
+          return allocateMemoryAndStoreRemaining(block,requestedSize);
+        }
       }
+
       else
       {
-        return createMoreMemory(tail,requestedSize);
+        if(block->next == 0)
+        {
+          //Case 3: We don't have enough memory in the free list. Thus, we have to create
+          //more memory.
+          return createMoreMemory(block,requestedSize);
+        }
+        else
+        {
+          block = block->next;
+        }
       }
     }
+  }
+
+  else
+  {
+    block_info* bestFit = block;
+    int foundAFit = 0;
+
+    while(block){
+      if(block->size >= requestedSize && block->availability==FREE)
+      {
+        //Determine if the free block is the best fit so far.
+        if(block -> size < bestFit -> size || bestFit -> size < requestedSize)
+        {
+          bestFit = block;
+          foundAFit = 1;
+        }
+      }
+      block = block -> next;
+    }
+
+    if(foundAFit)
+    {
+      //Case 1: It is not possible to split the free block into a smaller portion
+      //as there won't be enough memory to store the information about the free
+      //block. Thus, we return the block with excess memory.
+      if(bestFit->size - requestedSize <= 2*sizeof(block_info))
+      {
+        return allocateExtraMemory(bestFit,requestedSize);
+      }
+
+      //Case 2: We can allocated the requested memory and there is still enough memory
+      //left to store information about the segmented block size. Thus, we partition
+      //the available memory.
+      else
+      {
+        return allocateMemoryAndStoreRemaining(bestFit,requestedSize);
+      }
+    }
+    else
+    {
+      //Case 3: We don't have enough memory in the free list. Thus, we have to create
+      //more memory.
+      return createMoreMemory(tail,requestedSize);
+    }
+  }
 }
 
+/*
+This function frees the block of memory pointed to by pointer. Furthermore, if there
+are neighboring blocks that are free, the given block is coalesced with those.
+*/
 void my_free(void* ptr)
 {
+  //Get the metablocks for the neighbors to determine if they're available or not.
   block_info* currentSegmentTopMeta = (block_info *)(ptr - sizeof(block_info));
   block_info* currentSegmentBottomMeta = (block_info *)(ptr + currentSegmentTopMeta->size);
   block_info* topSegmentBottomMeta = (block_info *)ptr<head?NULL:(block_info*)(ptr - 2*sizeof(block_info));
@@ -223,7 +252,6 @@ void my_free(void* ptr)
     bottomSegmentBottomMeta -> address = topSegmentTopMeta -> address;
     bottomSegmentBottomMeta -> size = newSize;
     
-    
     //Update the pointers
     topSegmentTopMeta->next = bottomSegmentTopMeta->next;
     if(topSegmentTopMeta -> next){
@@ -297,11 +325,17 @@ void my_free(void* ptr)
   }
 }
 
+/*
+This function sets the policy to use while allocating memory.
+*/
 void my_mallopt(int policy)
 {
   policyToUse = policy;
 }
 
+/*
+This function prints statistics.
+*/
 void my_mallinfo()
 {
   printf("Total allocated bytes: %d (%d kB)\n",totalBytesAllocated,totalBytesAllocated/1024);
@@ -309,6 +343,11 @@ void my_mallinfo()
   printf("Largest free space: %d (%d kB)\n",largestFreeSpace,largestFreeSpace/1024);
 }
 
+/*
+This function handles Case 1 - itt is not possible to split the free block into a smaller portion
+as there won't be enough memory to store the information about the free
+block. Thus, we return the block with excess memory.
+*/
 void* allocateExtraMemory(block_info* block, int requestedSize)
 {
   block_info* bottomBlock; 
@@ -345,6 +384,11 @@ void* allocateExtraMemory(block_info* block, int requestedSize)
   return block->address;
 }
 
+/*
+This function handles Case 2 - We can allocated the requested memory and there is still enough memory
+left to store information about the segmented block size. Thus, we partition
+the available memory.
+*/
 void* allocateMemoryAndStoreRemaining(block_info* block, int requestedSize)
 {
   block_info* newSegmentedBlockInfo;
@@ -393,17 +437,25 @@ void* allocateMemoryAndStoreRemaining(block_info* block, int requestedSize)
   return block->address;
 }
 
+/*
+This function handles Case 3 - We don't have enough memory in the free list. Thus, we have to create
+more memory.
+*/
 void* createMoreMemory(block_info* block, int requestedSize)
 {
+  //Setup the top meta block.
   block_info* topBlock = (block_info *)sbrk(sizeof(block_info));
   topBlock->address = (void *)sbrk(requestedSize + EXTRA_MEMORY);
   topBlock->availability = FREE;
   topBlock->size = requestedSize + EXTRA_MEMORY;
 
+  //Setup the bottom meta block.
   block_info* bottomBlock = (block_info *)sbrk(sizeof(block_info));
   bottomBlock -> address = topBlock ->address;
   bottomBlock -> availability = FREE;
   bottomBlock -> size = requestedSize + EXTRA_MEMORY;
+  
+  //Adjust pointers.
   if(head -> availability == ALLOCATED){
     head = topBlock;
   }
@@ -419,6 +471,7 @@ void* createMoreMemory(block_info* block, int requestedSize)
   totalFreeBytes += requestedSize + EXTRA_MEMORY;
   largestFreeSpace = getLargestFreeSpace();
   
+  //Allocate from the memory created.
   if(block->size - requestedSize <= 2*sizeof(block_info))
   {
     return allocateExtraMemory(block,requestedSize);
@@ -429,6 +482,9 @@ void* createMoreMemory(block_info* block, int requestedSize)
   }
 }
 
+/*
+This function goes through the free list to find largest free block.
+*/
 int getLargestFreeSpace(){
   block_info* tmp = head;
   int largest = 0;
