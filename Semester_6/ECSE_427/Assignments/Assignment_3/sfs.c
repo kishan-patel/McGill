2b1b69302577d14cd0c62f2f871e0d315b32f71e
Kishan Patel <kishan@patelkishan.com>
2013-04-12 18:36:38 -0400

    OS PA3. mksfs and open work. write, read, and close left.

---
+++ Semester_6/ECSE_427/Assignments/Assignment_3/sfs.c
@@ -0,0 +1,239 @@
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <slack/std.h>
+#include <slack/list.h>
+#include "sfs_api.h"
+#include "disk_emu.h"
+
+#define MAX_FILES         100
+#define DIRECTORY_INDEX   0
+#define FAT_INDEX         1
+#define FREE_INDEX        2
+#define DESCRIPTOR_OFFSET 3
+#define BUSY              99
+#define FREE              100
+
+typedef struct{
+  char fileName[32];
+  int fileSize;
+  time_t dateModified;
+  int fatIndex; //Data block location of the given file.
+}directory_entry_t;
+
+typedef struct{
+  int fatIndex;
+  int writeIndex;
+  int readIndex;
+}fdt_t;
+
+typedef struct{
+  int blockIndex;
+  int nextFatIndex; //Next data block entry for a given file. Multiple data block entries are
+                    //used to represent a file.
+}fat_entry_t;
+
+static int blockSize;
+static int noBlocks;
+directory_entry_t* directory;
+fdt_t* fd;
+fat_entry_t* fat;
+List* freeList;
+list_release_t* destroyFreeList;
+int initialise = 1;
+int i;
+
+int mksfs(int fresh)
+{ 
+  initialize();
+  if(fresh){
+    init_fresh_disk("SFS",blockSize,noBlocks);
+    
+    //Reset all the structures
+    for(i = 0; i < MAX_FILES; i++)
+    {
+      //Reset directory entries.
+      strcpy(directory[i].fileName, "");
+      directory[i].fileSize = 0;
+      time(&directory[i].dateModified);
+      directory[i].fatIndex = 0;
+      
+      //Reset fdt entries
+      fd[i].fatIndex = 0;
+      fd[i].writeIndex = 0;
+      fd[i].readIndex = 0;
+      
+      //Reset the fat entries.
+      fat[i].blockIndex = 0;
+      fat[i].nextFatIndex = 0;
+      
+      //We store the blocks contain the disk data structures
+      //(i.e. root directory, fd, and fat). The rest of the blocks are free.
+      for(i=3;i<noBlocks;i++)
+      {
+        list_append_int(freeList,i);
+      }
+      
+      //Write the directory structure to the 1st block in the FS.
+      write_blocks(DIRECTORY_INDEX, 1, directory);
+      
+      //Write the fat to the 2nd block in the FS.
+      write_blocks(FAT_INDEX,1,fat);
+      
+      //Write the free list to the FS.
+      int* tmp = (int *)malloc(blockSize);
+      int block;
+      for(i=0; i<noBlocks; i++)
+      {
+        tmp[i] = BUSY;
+      }
+      for(i=0; i<list_length(freeList);i++)
+      {
+        block = list_item_int(freeList,i);
+        tmp[block] = FREE;
+      }
+      write_blocks(FREE_INDEX,1,tmp);
+      free(tmp);
+    }
+  }
+  return 0;
+}
+
+void sfs_ls(void)
+{
+  int currFileIndex = 0;
+  while((strcmp(directory[currFileIndex].fileName,""))){
+    printf("%s\n",directory[currFileIndex].fileName);
+    currFileIndex++;
+  }
+}
+
+int sfs_open(char *name)
+{
+  int found=0;
+  int currFileIndex = 0;
+  int openValue = -1;
+  
+  while(!found && currFileIndex < MAX_FILES){
+    if(!strcmp(directory[currFileIndex].fileName, name))
+    {
+      found=1;
+      break;
+    }
+    currFileIndex++;
+  }  
+  
+  //The file was found so we set up the appropriate entry in
+  //the fd table.
+  if(found)
+  {
+    fd[currFileIndex].fatIndex = directory[currFileIndex].fatIndex;
+    fd[currFileIndex].writeIndex = directory[currFileIndex].fileSize;
+    fd[currFileIndex].readIndex = 0;
+    openValue = currFileIndex;
+  }
+  
+  //The file was not found so we create one.
+  else
+  {
+     openValue = createFile(name);
+  }
+  
+  return openValue;
+}
+
+int sfs_close(int fileID)
+{
+  return 0;
+}
+
+int sfs_write(int fileID, char *buf, int length)
+{
+  return 0;
+}
+
+int sfs_read(int fileID, char *buf, int length)
+{
+  return 0;
+}
+
+void initialize()
+{
+  noBlocks = MAX_FILES + 3;
+  freeList = list_create(destroyFreeList);
+  blockSize = MAX_FILES * sizeof(directory_entry_t);
+  if(MAX_FILES * sizeof(fdt_t) > blockSize)
+  {
+    blockSize = MAX_FILES * sizeof(fdt_t);
+  }
+  else if(noBlocks * sizeof(fat_entry_t) > blockSize)
+  {
+    blockSize = noBlocks * sizeof(fat_entry_t);
+  }
+  
+  directory = (directory_entry_t*)malloc(blockSize);
+  fat = (fat_entry_t*)malloc(blockSize);
+  fd = (fdt_t*)malloc(MAX_FILES * sizeof(fdt_t));
+  fat[1].blockIndex = 1;
+  fat[2].blockIndex = 2;
+}
+
+int createFile(char *name)
+{
+  int newFileIndex = 0;
+  
+  //First we verify if there is space available to create this new file.
+  if(list_length(freeList) == 0)
+  {
+    return -1;
+  }
+  
+  //There is space so we update the directory table, fat, and fd table.
+  else
+  {
+    //Check where the next entry in the directory table goes.
+    while(directory[newFileIndex].fatIndex != 0)
+    {
+      newFileIndex++;
+    }
+    
+    //Update the directory table by putting an entry for the newly created file.
+    strcpy(directory[newFileIndex].fileName,name);
+    directory[newFileIndex].fileSize = 0;
+    time(&directory[newFileIndex].dateModified);
+    directory[newFileIndex].fatIndex = list_shift_int(freeList);
+    
+    //Update the fat by putting an entry for the newly created file.
+    fat[newFileIndex].blockIndex = directory[newFileIndex].fatIndex;
+    fat[newFileIndex].nextFatIndex = 0;
+    
+    //Update the fd table by putting an entry for the newly created file.
+    fd[newFileIndex].fatIndex = directory[newFileIndex].fatIndex;
+    fd[newFileIndex].readIndex = 0;
+    fd[newFileIndex].writeIndex = 0;
+    
+    //Write the directory structure to the 1st block in the FS.
+    write_blocks(DIRECTORY_INDEX, 1, directory);
+      
+    //Write the fat to the 2nd block in the FS.
+    write_blocks(FAT_INDEX,1,fat);
+      
+    //Write the free list to the FS.
+    int* tmp = (int *)malloc(blockSize);
+    int block;
+    for(i=0; i<noBlocks; i++)
+    {
+      tmp[i] = BUSY;
+    }
+    for(i=0; i<list_length(freeList);i++)
+    {
+      block = list_item_int(freeList,i);
+      tmp[block] = FREE;
+    }
+    write_blocks(FREE_INDEX,1,tmp);
+    free(tmp);
+  }
+  
+  return newFileIndex;
+}
---
+++ Semester_6/ECSE_427/Assignments/Assignment_3/sfs_api.h
@@ -0,0 +1,9 @@
+int mksfs(int fresh);
+void sfs_ls(void);
+int sfs_open(char *name);
+int sfs_close(int fileID);
+int sfs_write(int fileID, char *buf, int length);
+int sfs_read(int fileID, char *buf, int length);
+void initialize();
+int createFile(char *name);
+
