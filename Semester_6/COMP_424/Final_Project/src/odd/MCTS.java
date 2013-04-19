31fd4409b80619748a33111c3a6bfca6e93c9a8d
Kishan Patel <kishan@patelkishan.com>
2013-04-19 09:25:47 -0400

    Added comments. Going to merge to one file as per assignment specifications.

--- Semester_6/COMP_424/Final_Project/src/odd/MCTS.java
+++ Semester_6/COMP_424/Final_Project/src/odd/MCTS.java
@@ -16,16 +16,83 @@ public class MCTS {
 	private int totalRuns=0;
 	private OddBoard gameOddBoard;
 	
+	/**
+	 * Constructor.
+	 */
 	public MCTS(){
 		root = new MCTSNode(null);
 		currNode = root;
 		randomGen = new Random();
 	}
 	
+	/**
+	 * Sets the number that is associated with my turn.
+	 * @param myTurn
+	 */
 	public void setMyTurn(int myTurn){
 		this.myTurn = myTurn;
 	}
 	
+	/**
+	 * Returns the node associated with the current state (last move that was made).
+	 * @return
+	 */
+	public MCTSNode getCurrentStateNode(){
+		return currNode;
+	}
+	
+	/**
+	 * This method is used to determine what move the other player made. This is needed because we have 
+	 * to know where to continue building the MCST from.
+	 * @param oddBoard
+	 */
+	public void determineCurrentStateNode(OddBoard oddBoard){
+		List<MCTSNode>currNodeChildren;
+		Piece[][] data;
+		Piece prevPiece;
+		totalRuns = 0;
+		gameOddBoard = oddBoard;
+		
+		if(currNode.isLeaf() && currNode == root){
+			currNode.setBoardState(oddBoard);
+			addChildren(oddBoard, currNode);
+		}else{
+			currNodeChildren = currNode.getChildren();
+			data = currNode.getOddBoard().getBoardData();
+			
+			//To determine the other player's move, we know that they must have selected one of the moves 
+			//that are associated with the children of our current state. Thus, we go through each children,
+			//to know what the other player did.
+			for(MCTSNode child: currNodeChildren){
+				//Store the current piece at this position in case we need to revert.
+				prevPiece = data[child.getOddMove().destRow+4][child.getOddMove().destCol+4];
+				
+				//Add to the board, the move that is associated with the given child.
+				data[child.getOddMove().destRow+4][child.getOddMove().destCol+4] = child.getOddMove().color;
+				
+				if(dataIsSame(data,oddBoard.getBoardData())){
+					//If the board configurations match the configuration of the board that is passed to us then we
+					//know that the other player made the move that is associated with the child. Thus, we set the 
+					//current node to the child and continue building the tree from this node.
+					currNode = child;
+					currNode.setBoardState(oddBoard);
+					removeInvalidChildren(currNode, currNode.getOddBoard().getBoardData());
+					break;
+				}else{
+					//This is not the move the other player made so we revert.
+					data[child.getOddMove().destRow+4][child.getOddMove().destCol+4] = prevPiece;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Builds the MCST recursively. Starting from a given node, the method goes to the leaf and runs the simulation. After running,
+	 * the simulation, it propagates the scores. At each step, it determines the next node to select using the getBestSelection() method
+	 * which uses the UCB formula. 
+	 * @param oddBoard
+	 * @param node
+	 */
 	public void buildMCST(OddBoard oddBoard, MCTSNode node){
 		MCTSNode randomChild;
 		MCTSNode bestChild;
@@ -37,10 +104,6 @@ public class MCTS {
 			}
 			addChildren(node.getOddBoard(),node);
 			randomChild = getRandomChild(node);
-			/*if(!randomChild.isExpanded()){
-				oddBoard.move(randomChild.getOddMove());
-				randomChild.setExpanded(true);
-			}*/
 			gameOutcome = simulateGame(node.getOddBoard(),randomChild);
 			randomChild.incrementVisits();
 			randomChild.updateScore(gameOutcome);
@@ -55,82 +118,46 @@ public class MCTS {
 		}
 	}
 	
-	public OddMove getBestMove(){
+	/**
+	 * Gets the best move (move that will result in the highest chances of winning). The best move is determined,
+	 * by the score of each node that were set when the simulation was run.
+	 * @return
+	 */
+	public OddMove getBestMove() {
 		List<MCTSNode> children = currNode.getChildren();
 		float max = -Float.MAX_VALUE;
-	    int bestNodeIndex = randomGen.nextInt(children.size());
-	    float randomizer;
-	    MCTSNode child;
-	    //System.out.println("before");
-	    for (int i = 0; i < children.size(); i++) {
-	    	child = children.get(i);
-	    	float nodeScore = (float) child.getScore() / ((float) (child.getNoVisits() + Float.MIN_VALUE));
-	    	randomizer = Float.MIN_VALUE * randomGen.nextInt(children.size() * children.size());
-	    	if (nodeScore + randomizer > max) {
-	    		System.out.println("Highest node score = "+(nodeScore+randomizer));
-	    		System.out.println("Child visits = "+child.getNoVisits());
-	    		System.out.println("Child score = "+child.getScore());
-	    		max = nodeScore + randomizer;
-	    		bestNodeIndex = i;
-         }
-      }
-	      child = children.get(bestNodeIndex);
-	      OddBoard cBoard = (OddBoard)currNode.getOddBoard().clone();
-	      cBoard.move(child.getOddMove());
-	      child.setBoardState(cBoard);
-	      currNode = child;
-	      currNode.setParent(null);
-	      return currNode.getOddMove();
-	}
-	
-	public MCTSNode getCurrentStateNode(){
-		return currNode;
-	}
-	
-	public void determineCurrentStateNode(OddBoard oddBoard){
-		List<MCTSNode>currNodeChildren;
-		Piece[][] data;
-		Piece prevPiece;
-		boolean found = false;
-		totalRuns = 0;
-		/*if(currNode.isLeaf()){
-			addChildren(oddBoard,currNode);
-		}*/
-		gameOddBoard = oddBoard;
-		if(currNode.isLeaf() && currNode == root){
-			currNode.setBoardState(oddBoard);
-			addChildren(oddBoard, currNode);
-		}else{
-			if(currNode.isLeaf()){
-				addChildren(currNode.getOddBoard(), currNode);
-			}
-			//System.out.println("after");
-			currNodeChildren = currNode.getChildren();
-			data = currNode.getOddBoard().getBoardData();
-			for(MCTSNode child: currNodeChildren){
-				//System.out.println("x = " + child.getOddMove().destRow + ". y = "+ child.getOddMove().destCol + ".");
-				prevPiece = data[child.getOddMove().destRow+4][child.getOddMove().destCol+4];
-				data[child.getOddMove().destRow+4][child.getOddMove().destCol+4] = child.getOddMove().color;
-				if(dataIsSame(data,oddBoard.getBoardData())){
-					currNode = child;
-					currNode.setBoardState(oddBoard);
-					removeInvalidChildren(currNode, currNode.getOddBoard().getBoardData());
-					found = true;
-					break;
-				}else{
-					data[child.getOddMove().destRow+4][child.getOddMove().destCol+4] = prevPiece;
-				}
+		int bestNodeIndex = randomGen.nextInt(children.size());
+		float randomizer;
+		MCTSNode child;
+
+		for (int i = 0; i < children.size(); i++) {
+			child = children.get(i);
+			float nodeScore = (float) child.getScore() / ((float) (child.getNoVisits() + Float.MIN_VALUE));
+			randomizer = Float.MIN_VALUE * randomGen.nextInt(children.size() * children.size());
+			if (nodeScore + randomizer > max) {
+				max = nodeScore + randomizer;
+				bestNodeIndex = i;
 			}
 		}
 		
-		if(!found){
-			System.out.println("Actual");
-			System.out.println(currNode.getOddBoard());
-			System.out.println("\n\nExpected");
-			System.out.println(oddBoard);
-		}
+		//Update the board of the child node to the current node. And set the current node to the child node
+		//because this is where we are moving to.
+		child = children.get(bestNodeIndex);
+		OddBoard cBoard = (OddBoard) currNode.getOddBoard().clone();
+		cBoard.move(child.getOddMove());
+		child.setBoardState(cBoard);
+		currNode = child;
+		currNode.setParent(null);
+		
+		//Return the odd move that is associated with the child node.
+		return currNode.getOddMove();
 	}
 	
+	/**
+	 * Adds the children (valid moves) for a given node.
+	 * @param oddBoard
+	 * @param node
+	 */
 	private void addChildren(OddBoard oddBoard,MCTSNode node){
 		OddBoard tmp; 
 		List<OddMove> validMoves = node.getOddBoard().getValidMoves();
@@ -145,6 +172,11 @@ public class MCTS {
 		node.setIsLeaf(false);
 	}
 	
+	/**
+	 * Randomly picks a child and returns it from the list of available children.
+	 * @param node
+	 * @return
+	 */
 	private MCTSNode getRandomChild(MCTSNode node){
 		if(node.getChildren().size() == 0){
 			return null;
@@ -153,60 +185,49 @@ public class MCTS {
 		return node.getChildren().get(index);
 	}
 	
+	/**
+	 * Runs a game from the current node until the end of the game is reached. At each turn, it picks
+	 * a random node from the list of valid nodes.
+	 * @param oddBoard
+	 * @param node
+	 * @return
+	 */
 	private int simulateGame(OddBoard oddBoard, MCTSNode node){
 		OddBoard clonedBoard = (OddBoard)oddBoard.clone();
 		List<OddMove> validMoves;
-		int turn, gameOutcome;
-		boolean moveMade = false;
+		int  gameOutcome;
 		OddMove move = null;
-		Piece prevPiece;
-		Piece[][] clonedData = clonedBoard.getBoardData();
-		turn = oddBoard.getTurn();
 		int emptyPositions = clonedBoard.countEmptyPositions();
 		
-		//Run simulation until the game is over. At each step, greedily pick 
-		//a move that will result in a win for the given player.
+		//Run simulation until the game is over. At each step, pick a random valid move 
+		//and execute it.
 		while(emptyPositions!=0){
-			//turn = clonedBoard.getTurn();
 			validMoves = clonedBoard.getValidMoves();
-			/*for(OddMove oddMove: validMoves){
-				moveMade = false;
-				move = oddMove;
-				prevPiece = clonedData[oddMove.destRow + OddBoard.SIZE][oddMove.destCol + OddBoard.SIZE];
-				clonedData[oddMove.destRow + OddBoard.SIZE][oddMove.destCol + OddBoard.SIZE] = oddMove.color;
-				clonedBoard.determineWinner();
-				if(clonedBoard.getWinner() == turn){
-					moveMade = true;
-					turn = (turn == 1)?2:1;
-					emptyPositions--;
-					break;
-				}else{
-					clonedData[oddMove.destRow + OddBoard.SIZE][oddMove.destCol + OddBoard.SIZE] = prevPiece;
-				}
-			}
-			if(!moveMade){
-				clonedData[move.destRow + OddBoard.SIZE][move.destCol + OddBoard.SIZE] = move.color;
-				turn = (turn == 1)?2:1;
-				emptyPositions--;
-				clonedBoard.determineWinner();
-			}*/
 			move = validMoves.get(randomGen.nextInt(validMoves.size()));
 			clonedBoard.move(move);
 			emptyPositions = clonedBoard.countEmptyPositions();
 		}
+		
+		//The game is over so we determine the winner and return the results.
+		totalRuns++;
 		clonedBoard.determineWinner();
-		//Set the game outcome and return it.
 		if(clonedBoard.getWinner() == myTurn){
 			 gameOutcome = WIN;
 		}else{
 			gameOutcome = LOSE;
 		}
-		totalRuns++;
 		return gameOutcome;
 	}
 	
+	/**
+	 * Based on the game outcome, the parent scores are updated. The number of times, this branch
+	 * has been played is also updated.
+	 * @param node
+	 * @param gameOutcome
+	 */
 	private void updateParentScores(MCTSNode node,int gameOutcome){
 		MCTSNode parent = node.getParent();
+		
 		while(parent != null){
 			parent.incrementVisits();
 			parent.updateScore(gameOutcome);
@@ -214,30 +235,28 @@ public class MCTS {
 		}
 	}
 	
+	/**
+	 * Returns the best child (arm) from which we should run our simulation. It uses the UCB formula
+	 * to determine this node.
+	 * @param node
+	 * @param turn
+	 * @return
+	 */
 	private MCTSNode getBestSelection(MCTSNode node,int turn){
-		if(turn == myTurn){
-			turn = 1;
-		}else{
-			turn = -1;
-		}
 		List<MCTSNode> children = node.getChildren();
 		MCTSNode childNode;
 		int bestNodeIndex = 0;
-		float childScore,bias,randomizer,biasedScore;
+		float childScore;
 		float max = -Float.MAX_VALUE;
 		
 		//Find the index of the best node.
 		for(int i = 0; i<children.size(); i++){
 			childNode = children.get(i);
-			/*childScore = (float)childNode.getScore()/((float)(childNode.getNoVisits() + Float.MIN_VALUE));
-			bias = 2 * (float) (Math.sqrt(Math.log((float) node.getNoVisits()) / ((float) childNode.getNoVisits() + Float.MIN_VALUE)));
-			randomizer = Float.MIN_VALUE * randomGen.nextInt(children.size() * children.size());
-			biasedScore = childScore + randomizer + (bias * turn);*/
 			if(!childNode.isExpanded()){
 				childNode.setExpanded(true);
 				return childNode;
 			}
-			
+			//UCB formula is employed to balance between exploration and exploitation.
 			childScore = (float) (childNode.getScore()/(childNode.getNoVisits()) + Math.sqrt(2*Math.log(totalRuns)/(5*childNode.getNoVisits())));
 			if(childScore > max){
 				max = childScore;
@@ -249,6 +268,12 @@ public class MCTS {
 		return children.get(bestNodeIndex);
 	}
 	
+	/**
+	 * Checks whether the data (pieces on the board) is the same for the two arrays.
+	 * @param data1
+	 * @param data2
+	 * @return
+	 */
 	private boolean dataIsSame(Piece[][]data1, Piece[][]data2){
 		for(int i=0;i<data1.length;i++){
 			for(int j=0;j<data1.length;j++){
@@ -260,24 +285,27 @@ public class MCTS {
 		return true;
 	}
 	
+	/**
+	 * Based on the current state of the game, this method removes children that are no longer represent
+	 * a valid move.
+	 * @param node
+	 * @param latestBoard
+	 */
 	private void removeInvalidChildren(MCTSNode node, Piece[][]latestBoard){
 		List<MCTSNode>children = node.getChildren();
 		OddMove oddMove;
 		List<MCTSNode>nodesToRemove = new ArrayList<MCTSNode>();
-		/*for(int i = 0; i<children.size(); i++){
-			oddMove = children.get(i).getOddMove();
-			if(latestBoard[oddMove.destRow + OddBoard.SIZE][oddMove.destCol + OddBoard.SIZE] != Piece.EMPTY){
-				children.remove(i);
-			}
-		}*/
+		
 		for(MCTSNode child: children){
 			oddMove = child.getOddMove();
 			if(latestBoard[oddMove.destRow + OddBoard.SIZE][oddMove.destCol + OddBoard.SIZE] != Piece.EMPTY){
+				//Store the children to remove in a list.
 				nodesToRemove.add(child);
 			}
 		}
 		
 		for(MCTSNode child: nodesToRemove){
+			//Remove all children that were added in the previous loop.
 			children.remove(child);
 		}
 	}
